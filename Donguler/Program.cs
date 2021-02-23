3b298a35d19cc87c2a66b656b1565c0c3e3cb8e1
Büşra Çoğul <77806575+busracogul@users.noreply.github.com>
2021-02-23 14:42:40 +0300

    Add files via upload

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "python";
+            string kurs5 = "C++";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+
+
+            Console.WriteLine("-------------------------");
+
+            //array-dizi
+
+            string[] kurslar = new string[] { "yazılım geliştirici yetiştirme kampı",
+                "programlamaya başlangıç için temel kurs","Java","python","C#"};
+            
+
+            for (int i = 0; i < kurslar.Length; i++)
+            { 
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti");
+
+            //foreach-dizileri tek tek dolaşmaya yarar
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            // deger tutucu,alias 
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+            
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
